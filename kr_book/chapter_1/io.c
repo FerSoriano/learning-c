e5e8caaea1f34cadb5c2da1c8ba9356b15ed6172
FerSoriano <fer8f10@hotmail.com>
2025-12-10 20:10:12 -0600

    start K&R book

--- kr_book/chapter_1/io.c
+++ kr_book/chapter_1/io.c
@@ -10,10 +10,13 @@ int main() {
     */
     int c;
 
+    // empieza a leer por consola, almacenando en buffer cada char
     c = getchar();
+
+    // al presionar enter, empieza la ejecucion del bucle
     while (c != EOF)
     {
-        putchar(c);
+        putchar(c); // imprime por consola cada char del buffer
         c = getchar();
     }
 }
